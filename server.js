077db9c8e61b52fd96d149f71d1c53a56232a195
Bhaskar Reddy <babubhaskar791@gmail.com>
2023-01-22 11:40:08 +0530

    intial setup -1

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, world!');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
